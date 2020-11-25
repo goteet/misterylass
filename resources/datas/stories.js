ba93050d69628db5d48b016e0414a8cbe69d8cc0
zhuangyuhe <zhuangyuhe@tencent.com>
2020-11-25 20:51:53 +0800

    story line basis

---
+++ resources/datas/stories.js
@@ -0,0 +1,16 @@
+const kStoryLines = {
+    nodes:{
+        "newbie_swordman_01": "hello world",
+        "newbie_swordman_02": "hello again",
+        "newbie_mage_01": "hello there",
+        "newbie_mage_02": "hello peee",
+    },
+    links:{
+        "newbie_swordman_01":"newbie_swordman_02",
+        "newbie_mage_01":"newbie_mage_02",
+    },
+    entrances:[
+        "newbie_swordman_01",
+        "newbie_mage_01"
+    ]
+};
