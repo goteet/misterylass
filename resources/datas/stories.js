fa9480addcee17ef7a80ce31d013d2ccdc879ece
zhuangyuhe <zhuangyuhe@tencent.com>
2020-11-25 22:31:48 +0800

    potion efficiency condition

--- resources/datas/stories.js
+++ resources/datas/stories.js
@@ -1,13 +1,54 @@
+const kNodeBuyPotion = 0;
+const kNodeSellIngredient = 1;
+const kNodePotionFeedBack = 2;
+const kNodeEventFeedBack = 3;
+const kNodeConversation = 4;
+
 const kStoryLines = {
     nodes:{
-        "newbie_swordman_01": "hello world",
-        "newbie_swordman_02": "hello again",
-        "newbie_mage_01": "hello there",
-        "newbie_mage_02": "hello peee",
+        "newbie_swordman_01":{
+            type:kNodeBuyPotion,
+            text:"hello world!"
+        },
+        "newbie_swordman_02_full":{
+            type:kNodeBuyPotion,
+            text:"good!"
+        },
+        "newbie_swordman_02_norm":{
+            type:kNodeBuyPotion,
+            text:"just soso."
+        },
+        "newbie_swordman_02_half":{
+            type:kNodeBuyPotion,
+            text:"critical wound"
+        },
+        "newbie_swordman_02_dead":{
+            type:kNodeBuyPotion,
+            text:"i am a dead man."
+        },
+        "newbie_swordman_02":{
+            type:kNodeBuyPotion,
+            text:"hello again!"
+        },
+        "newbie_mage_01":{
+            type:kNodeBuyPotion,
+            text:"hello there..."
+        },
+        "newbie_mage_02":{
+            type:kNodeBuyPotion,
+            text:"hello peeee..."
+        }
     },
     links:{
-        "newbie_swordman_01":"newbie_swordman_02",
-        "newbie_mage_01":"newbie_mage_02",
+        "newbie_swordman_01":[
+            { efficiency:60, next:"newbie_swordman_02_full" },
+            { efficiency:40, next:"newbie_swordman_02_norm" },
+            { efficiency:25, next:"newbie_swordman_02_half" },
+            { next:"newbie_swordman_02_dead" }
+        ],
+        "newbie_mage_01":[
+            { next:"newbie_mage_02" }
+        ]
     },
     entrances:[
         "newbie_swordman_01",
--- scripts/index.js
+++ scripts/index.js
@@ -8,44 +8,72 @@ const kWeekDayString = [
     "土曜日",
 ];
 
+const RandomMax = function(max){
+    return Math.floor(Math.random() * (max));
+}
+
+const RandomRange = function(min,max){
+    return min + Math.floor(Math.random() * (max-min+1));
+}
+
 const RootComponent = {
     data:{
-        today : 0,
+        today:0,
+        potionEfficiency:100,
         existStoryLines:[],
     },
     mounted:function(){
-        this.advanceStoryLine();
+        this.pickNewStoryLine();
     },
     computed: {
         CurrentDay: function() { return kWeekDayString[this.today]; },
         MainDescription: function() {
-            return kStoryLines.nodes[this.existStoryLines[0]];
+            if(this.existStoryLines[0] == undefined)
+                return "";
+            let node = kStoryLines.nodes[this.existStoryLines[0]];
+            return node.text;
         }
     },
     methods: {
-        goNextDay: function() {
+        goNextDayRandom:function(){
+            let potionEfficiency = RandomRange(1, 100);
+            this.goNextDayWith(potionEfficiency);
+        },
+        goNextDayWith: function(efficiency) {
             this.today = (this.today + 1) % 7; 
+            this.potionEfficiency = efficiency;
             this.advanceStoryLine();
         },
         advanceStoryLine: function() {
-            if(this.existStoryLines.length != 0) {
-                if(kStoryLines.links.hasOwnProperty(this.existStoryLines[0])) {
-                    let currentStoryLineId = this.existStoryLines[0];
-                    this.existStoryLines.length = 0;
-                    this.existStoryLines.push(kStoryLines.links[currentStoryLineId]);
+            if(this.existStoryLines.length == 0) {
+                this.pickNewStoryLine();
+                return;
+            }
+            
+            let currentStoryLineId = this.existStoryLines[0];
+            if(!kStoryLines.links.hasOwnProperty(currentStoryLineId)) {
+                this.existStoryLines.length = 0;
+                this.pickNewStoryLine();
+                return;
+            }
+
+            let links = kStoryLines.links[currentStoryLineId];
+            this.existStoryLines.length = 0;
+            for(let i = 0; i < links.length; ++i){
+                if(!links[i].hasOwnProperty("efficiency")) {
+                    this.existStoryLines.push(links[i].next);
                     return;
-                } else {
-                    this.existStoryLines.length = 0;
-                    this.pickNewStoryLine();
+                }
+                else if(links[i].efficiency <= this.potionEfficiency){
+                    this.existStoryLines.push(links[i].next);
+                    return;
                 }
             }
-            else{
-                this.pickNewStoryLine();
-            }
+            this.pickNewStoryLine();
         },
         pickNewStoryLine:function(){
             let entranceCount = kStoryLines.entrances.length;
-            let pickIndex = Math.floor(Math.random() * entranceCount);
+            let pickIndex = RandomMax(entranceCount);
             let nodeId = kStoryLines.entrances[pickIndex];
             this.existStoryLines.length = 0;
             this.existStoryLines.push(nodeId);
